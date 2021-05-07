19ad3c5bf9ec71ad6b503a49a0e7846f70701ad4
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2021-05-07 16:09:21 +0530

    speed

--- src/StoryOrb.js
+++ src/StoryOrb.js
@@ -20,7 +20,7 @@ const StoryOrb = (props) => {
         return (
             <motion.div initial={{ opacity: 0, scale: 0.25 }}
                 animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 1 }}
+                transition={{ duration: 0.5 }}
                 className='story-orb' onClick={clickHandle}>
                 <img src={storyUrl} alt="" />
             </motion.div>
